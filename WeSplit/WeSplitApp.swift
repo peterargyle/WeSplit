26327a9ead63d6e486bb0dc42d8ced8a656c170e
Peter Argyle <peterargyle@mac.com>
2024-01-10 19:49:03 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Local Admin on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
